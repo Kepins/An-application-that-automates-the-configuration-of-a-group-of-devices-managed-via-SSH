b09e154930b9d5f6c24218b833331588b8e5f0aa
Your Name <dawid.rezmer1@gmail.com>
2023-08-02 13:52:12 +0200

    KSSH-18:Add README.md

--- ssh_configuration_app/settings.py
+++ ssh_configuration_app/settings.py
@@ -19,7 +19,7 @@ env = Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+Env.read_env(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
