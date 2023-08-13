4b02798f8c4f60ede2b317e8f796daa54a2679ff
szymon.kepinski <sz.kepinski1@gmail.com>
2023-08-13 23:17:13 +0200

    KSSH-5: Install Django Rest Framework

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "config.application.apps.ApplicationConfig",
 ]
 
@@ -87,6 +88,9 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
