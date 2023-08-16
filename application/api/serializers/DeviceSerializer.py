965d54916cb39ec64c7ec075399c1c1ea733501b
szymon.kepinski <sz.kepinski1@gmail.com>
2023-08-16 19:25:12 +0200

    KSSH-5: Replace key_content in DeviceSerializer with public_key(pk)

--- application/api/serializers/DeviceSerializer.py
+++ application/api/serializers/DeviceSerializer.py
@@ -5,19 +5,7 @@ from application.models.Device import Device, PublicKey
 
 class DeviceSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
-    key_content = serializers.SlugRelatedField(
-        slug_field="key_content",
-        queryset=PublicKey.objects.all(),
-        required=False,
-        source="public_key",
-        many=False,
-    )
 
     class Meta:
         model = Device
-        fields = [
-            "id",
-            "name",
-            "hostname",
-            "key_content",
-        ]
+        fields = "__all__"
--- application/api/views/DeviceList.py
+++ application/api/views/DeviceList.py
@@ -1,40 +1,10 @@
-from rest_framework import generics, status
-from rest_framework.response import Response
+from rest_framework import generics
 
 
 from application.api.serializers.DeviceSerializer import DeviceSerializer
-from application.api.serializers.PublicKeySerializer import PublicKeySerializer
 from application.models.Device import Device
-from application.models.PublicKey import PublicKey
 
 
 class DeviceList(generics.ListCreateAPIView):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(
-            data={"name": request.data["name"], "hostname": request.data["hostname"]},
-            partial=True,
-        )
-        serializer.is_valid(raise_exception=True)
-
-        if (
-            "key_content" in request.data
-            and not PublicKey.objects.filter(
-                key_content=request.data["key_content"]
-            ).exists()
-        ):
-            public_key = PublicKeySerializer(
-                data={"key_content": request.data["key_content"]}
-            )
-            public_key.is_valid(raise_exception=True)
-            public_key.save()
-
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
