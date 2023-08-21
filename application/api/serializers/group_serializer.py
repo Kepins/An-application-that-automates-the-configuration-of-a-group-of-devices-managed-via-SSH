ce623ae10ae3d3d026cb26bd9df597881941ada9
szymon.kepinski <sz.kepinski1@gmail.com>
2023-08-21 14:17:19 +0200

    KSSH-6: Add removing devices from group without tests

--- application/api/serializers/group_serializer.py
+++ application/api/serializers/group_serializer.py
@@ -31,3 +31,22 @@ class GroupAddDevicesSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "devices": {"required": True},
         }
+
+
+class GroupRemoveDevicesSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+    name = serializers.ReadOnlyField()
+    public_key = serializers.ReadOnlyField()
+
+    def validate_devices(self, devices):
+        if any(device not in self.instance.devices.all() for device in devices):
+            raise serializers.ValidationError("At least one device not in group")
+        out_devices = [dev for dev in self.instance.devices.all() if dev not in devices]
+        return out_devices
+
+    class Meta:
+        model = Group
+        fields = "__all__"
+        extra_kwargs = {
+            "devices": {"required": True},
+        }
--- application/api/views/group.py
+++ application/api/views/group.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from application.api.serializers.group_serializer import (
     GroupSerializer,
     GroupAddDevicesSerializer,
+    GroupRemoveDevicesSerializer,
 )
 from application.models.group import Group
 
@@ -14,14 +15,25 @@ class GroupKeyViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     serializer_add_devices = GroupAddDevicesSerializer
+    serializer_remove_devices = GroupRemoveDevicesSerializer
 
     def get_serializer_class(self):
         if self.action == "add_devices":
             return self.serializer_add_devices
+        if self.action == "remove_devices":
+            return self.serializer_remove_devices
         return self.serializer_class
 
     @action(detail=True, methods=["POST"])
     def add_devices(self, request, pk):
+        serializer = self.get_serializer(self.get_object(), data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    @action(detail=True, methods=["POST"])
+    def remove_devices(self, request, pk):
         serializer = self.get_serializer(self.get_object(), data=request.data)
         serializer.is_valid(raise_exception=True)
 
