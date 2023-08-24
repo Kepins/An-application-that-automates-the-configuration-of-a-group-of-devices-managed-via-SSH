a2ebc95132f06c7a9125f41d94e1ec3cbe5927fb
szymon.kepinski <sz.kepinski1@gmail.com>
2023-08-24 22:55:26 +0200

    KSSH-101: Tests for run_script endpoint

--- application/api/views/run_script.py
+++ application/api/views/run_script.py
@@ -2,18 +2,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from application.api.serializers.run_serializer import RunSerializer
-from application.models import Group
 from application.tasks.run_script import run_script_on_device
 
 
-class RunAPIView(APIView):
-
+class RunScriptAPIView(APIView):
     def post(self, request, format=None):
         serializer = RunSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         group = serializer.validated_data["group"]
         for device in group.devices.all():
-            run_script_on_device.delay(group=group.id, device=device.id, script=serializer.validated_data["script"].id)
+            run_script_on_device.delay(
+                group=group.id,
+                device=device.id,
+                script=serializer.validated_data["script"].id,
+            )
 
         return Response(serializer.data)
--- config/urls.py
+++ config/urls.py
@@ -12,7 +12,7 @@ from application.api.views.device import DeviceViewSet
 from application.api.views.key_pair import KeyPairViewSet
 from application.api.views.group import GroupKeyViewSet
 from application.api.views.custom_user import RegisterView
-from application.api.views.run import RunAPIView
+from application.api.views.run_script import RunScriptAPIView
 from application.api.views.script import ScriptViewSet
 
 router = DefaultRouter()
@@ -26,6 +26,6 @@ urlpatterns = [
     path("auth/login/", TokenObtainPairView.as_view(), name="login"),
     path("auth/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api/", include((router.urls, "api"), namespace="api")),
-    path("api/run/", RunAPIView.as_view(), name="run"),
+    path("api/run_script/", RunScriptAPIView.as_view(), name="run_script"),
     path("auth/register/", RegisterView.as_view(), name="register"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
