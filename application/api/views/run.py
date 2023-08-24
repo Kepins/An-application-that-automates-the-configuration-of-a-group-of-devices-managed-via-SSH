c32f3b33ac6dc639616dc48ba75b1038a759288f
szymon.kepinski <sz.kepinski1@gmail.com>
2023-08-24 21:15:50 +0200

    KSSH-101: Add run script endpoint

---
+++ application/api/views/run.py
@@ -0,0 +1,19 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from application.api.serializers.run_serializer import RunSerializer
+from application.models import Group
+from application.tasks.run_script import run_script_on_device
+
+
+class RunAPIView(APIView):
+
+    def post(self, request, format=None):
+        serializer = RunSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        group = serializer.validated_data["group"]
+        for device in group.devices.all():
+            run_script_on_device.delay(group=group.id, device=device.id, script=serializer.validated_data["script"].id)
+
+        return Response(serializer.data)
