af971384c0fcf806f0d88c622bbad0d35ab0604d
dawid.rezmer <dawid.rezmer@profil-software.com>
2023-09-03 18:19:45 +0200

    KSSH-201: Comment using CustomUser

--- application/utils/task_utils.py
+++ application/utils/task_utils.py
@@ -7,7 +7,7 @@ from application.models import Device, Group, CustomUser
 
 def create_connection(device_pk, group_pk):
     group = Group.objects.get(pk=group_pk)
-    user = CustomUser.objects.first()
+    # user = CustomUser.objects.first() when authentification will work
     device = Device.objects.get(pk=device_pk)
     if key_pair := device.key_pair:
         try:  # try connect using device's key
@@ -43,19 +43,20 @@ def create_connection(device_pk, group_pk):
                 )
             except SSHException:
                 print("Wrong private key")
-    if key_pair3 := user.key_pair:
-        if key_pair3 not in [key_pair, key_pair2]:
-            try:  # try connect using user's key if it is different from device's key and group's key
-                key_content = key_pair2.private_key_content
-                private_key = RSAKey(file_obj=StringIO(key_content))
-                return Connection(
-                    host=device.hostname,
-                    user=device.username,
-                    port=device.port,
-                    connect_kwargs={"pkey": private_key, "timeout": 10},
-                )
-            except SSHException:
-                print("Wrong private key")
+    # we need working authentification for code below
+    # if key_pair3 := user.key_pair:
+    #    if key_pair3 not in [key_pair, key_pair2]:
+    #        try:  # try connect using user's key if it is different from device's key and group's key
+    #            key_content = key_pair2.private_key_content
+    #            private_key = RSAKey(file_obj=StringIO(key_content))
+    #            return Connection(
+    #                host=device.hostname,
+    #                user=device.username,
+    #                port=device.port,
+    #                connect_kwargs={"pkey": private_key, "timeout": 10},
+    #            )
+    #        except SSHException:
+    #            print("Wrong private key")
     else:  # password and key_pair not specified
         print("No device and key_pair specified")
         return False
