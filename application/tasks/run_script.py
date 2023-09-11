3782fd811ff0094fd41892841da0834a83f84c91
dawid.rezmer <dawid.rezmer@profil-software.com>
2023-09-11 21:39:11 +0200

    KSSH-201: Completed __init__.py in serializers folder

--- application/tasks/run_script.py
+++ application/tasks/run_script.py
@@ -2,12 +2,21 @@ from celery import shared_task
 from invoke import UnexpectedExit
 from paramiko import AuthenticationException
 
+from application.api.serializers import Run
 from application.models import Script
 from application.utils.task_utils import create_connection, check_connection
 
 
 @shared_task(ignore_result=True)
 def run_script_on_device(group_pk, device_pk, script_pk):
+    response = ConnStatusSerializer(
+        partial=True,
+        data={
+            "request_uuid": request_uuid,
+            "device": device_id,
+            "warnings": [],
+        },
+    )
     status, warns, password, key = check_connection(device_pk, group_pk)
 
     script = Script.objects.get(pk=script_pk)
