a59a00644f8c19e731e73258f0d7e4beaa1844d1
dawid.rezmer <dawid.rezmer@profil-software.com>
2023-10-26 17:59:36 +0200

    KSSH-211: Remove KeyPair from CustomUser

--- application/models/custom_user.py
+++ application/models/custom_user.py
@@ -1,11 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-from .key_pair import KeyPair
 
 
 class CustomUser(AbstractUser):
 
-    """Key used for ssh authorization"""
-
-    key_pair = models.ForeignKey(KeyPair, null=True, on_delete=models.SET_NULL)
+    pass
--- config/settings/base.py
+++ config/settings/base.py
@@ -19,7 +19,6 @@ env = Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
