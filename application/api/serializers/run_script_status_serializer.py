d35e6de869c2ce390e13ec329643eadff447c327
Kepins <sz.kepinski1@gmail.com>
2023-11-29 20:11:34 +0100

    KSSH-115: Catch IndexError

--- application/api/serializers/run_script_status_serializer.py
+++ application/api/serializers/run_script_status_serializer.py
@@ -6,9 +6,7 @@ from application.utils.task_utils import RunScriptStatus
 class RunScriptStatusSerializer(serializers.Serializer):
     request_uuid = serializers.UUIDField(required=False)
     device = serializers.IntegerField(min_value=1)
-    status = serializers.ChoiceField(
-        choices=[(e.value, e.name) for e in RunScriptStatus]
-    )
+    status = serializers.CharField()
     warnings = serializers.ListField(child=serializers.CharField())
     result_std = serializers.CharField(allow_blank=True)
     result_err = serializers.CharField(allow_blank=True)
